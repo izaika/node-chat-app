5325774b7050fde15f98722e9dc9909b48a86d7f
Igor Zaika <izaika@Igors-MacBook-Pro.local>
2017-04-25 21:53:54 +0300

    add styles

--- public/js/index.js
+++ public/js/index.js
@@ -45,12 +45,16 @@ messageForm.on('submit', function (e) {
 locationButton.on('click', function (e) {
 	if (!navigator.geolocation) return alert('Geolocation is not supported by your browser.');
 
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition(function (position) {
+		locationButton.removeAttr('disabled').text('Send location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude,
 		});
 	}, function () {
+		locationButton.removeAttr('disabled').text('Send location');
 		alert('Unable to fetch location.');
 	});
 });
